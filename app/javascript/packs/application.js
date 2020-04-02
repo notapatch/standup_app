25654df8d33522bc8a76ef349940c54db7d6461f
Richard <richard.wigley@gmail.com>
2020-04-02 12:28:00 +0100

    3.4.2.2 jQuery, Pooper.js and Tether globally available

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "@fortawesome/fontawesome-free/js/all";
+var jQuery = require("jquery")
 
+// import JQuery from "jquery"
+global.$ = global.jQuery = jQuery;
+window.$ = window.JQuery = jQuery;
+
+require("bootstrap");
+require("admin-lte");
+
+import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/application.scss"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
