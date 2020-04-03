afa975479523e3000c1f3ed8919263b3ca788938
Richard <richard.wigley@gmail.com>
2020-04-03 19:06:50 +0100

    4.1.4.3 Flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(_opts = {})
+    # return if flash[0][0] == :form_error
+    flash.each do |msg_type, message|
+      next if msg_type == :form_error
+      concat(content_tag(:div, message, {
+        class: "alert #{bootstrap_class_for(msg_type)} fade show"
+      }) {
+               concat content_tag(:button, "x", class: "close", data: {
+                 dismiss: "alert"
+               })
+               concat message
+             })
+    end
+    nil
+  end
 end
