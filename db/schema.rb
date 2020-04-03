6b66324cec81d353576042fdc058a2b416e917bc
Richard <richard.wigley@gmail.com>
2020-04-03 10:55:56 +0100

    4.1.2 rails g migration AddTimeZoneToUsers time_zone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_091945) do
+ActiveRecord::Schema.define(version: 2020_04_03_092651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_091945) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "time_zone"
   end
 
 end
