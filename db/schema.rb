608ff54c93bcb4e352ae1f7ab6026cf643df1b8d
Richard <richard.wigley@gmail.com>
2020-04-04 17:25:25 +0100

    4.1.4.4 Relationship account --* users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_181249) do
+ActiveRecord::Schema.define(version: 2020_04_03_183212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -40,8 +40,11 @@ ActiveRecord::Schema.define(version: 2020_04_03_181249) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.uuid "account_id"
+    t.index ["account_id"], name: "index_users_on_account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "accounts"
 end
