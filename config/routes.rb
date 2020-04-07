60367ffc0ef87372f64275b165313d5cde03d8a4
Richard <richard.wigley@gmail.com>
2020-04-07 16:06:58 +0100

    UsersController remove code prior to adding actions back

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/create"
-  get "users/edit"
-  get "users/update"
   devise_for :users, controllers: {registrations: "registrations"}
   resource :accounts
 
